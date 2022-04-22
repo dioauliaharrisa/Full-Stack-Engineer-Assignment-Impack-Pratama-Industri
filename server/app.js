48c8b90a14ab7b2ee97d7e46565434c27f5ea052
Dio Harrisa <daharrisa@gmail.com>
2022-04-22 23:33:55 +0700

    done creating skeleton server

---
+++ server/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+app.listen(PORT, () => {
+  console.log(`Impack Pratama server is running on port ${PORT}.`);
+});
